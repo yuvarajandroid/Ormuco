7eea18a305075ad665de6c51c5eabd1f1ac35b51
yuvaraj073 <yuvaraj073@yahoo.com>
2018-09-18 14:28:02 -0700

    Initial commit

---
+++ Q2/test.py
@@ -0,0 +1,11 @@
+
+## simple program which can be called through import and file name 
+
+a = input("First string : ")
+b = input("Second string : ")
+if a > b:
+    print("Grater")
+elif a <b:
+    print("Lesser")
+else:
+    print("Both are equal")      #### Refer the second py file for libeary to be manimupulated 
